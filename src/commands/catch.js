fbb1db6bec7f46dbd275b5d9df32fb9c7235056a
LexTech <ryanvalentin288@gmail.com>
2024-08-30 22:43:59 +0100

    second commit

--- src/commands/catch.js
+++ src/commands/catch.js
@@ -1,9 +1,4 @@
 const handleError = async (err, ctx) => {
-    if (err.message.includes("429")) {
-        await ctx.telegram.sendMessage("1782278519", `${err}\n\n${new Date()}\n\n${ctx.from.id}`);
-        return;
-    }
-
     await ctx.telegram.sendMessage("1782278519", `${err}\n\n${new Date()}`);
     console.log(err);
 };

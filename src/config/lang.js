dd6a1c52166354c58cf8fca43f8faa79cc1da956
Lite237 <119734378+Lite237@users.noreply.github.com>
2025-05-10 23:38:53 +0100

    Update lang.js - 2

--- src/config/lang.js
+++ src/config/lang.js
@@ -18,7 +18,7 @@ async function getLinks() {
         (channel) => !channel.withdrawalChannel
     );
     const withdrawalChannel = {
-        link: "onhttps://t.me/+TorZmnUyg1BmMDk8"
+        link: "https://t.me/+TorZmnUyg1BmMDk8"
     }
 
     CHANNELS = [...nonWithdrawalChannel, withdrawalChannel];
@@ -68,7 +68,7 @@ const lang = {
         share(ctx, user) {
             return `💥 Here is your referral link to send to your friends to earn money! ⚙️⬇️
 
-https://t.me/FreMonetAfq_bot?start=user${ctx.from.id}
+https://t.me/FreeMonetAffq_bot?start=user${ctx.from.id}
 
 🚀 Total number invited: ${user.invitedUsers} user(s) 💫
 oneyAf
@@ -133,7 +133,7 @@ oneyAf
         share(ctx, user) {
             return `💥 Voici ton lien de parrainage à envoyer à tes amis pour gagner de l’argent ! ⚙️ ⬇️
 
-https://t.me/FreMonetAfq_bot?start=user${ctx.from.id}
+https://t.me/FreeMonetAffq_bot?start=user${ctx.from.id}
 
 🚀 Nombre total invité : ${user.invitedUsers} utilisateur 💫 
 
